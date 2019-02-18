23c7e89a02add40e70ee6eaed99bb98784a05be4
simo085f <simo085f@stud.kea.dk>
2019-02-18 11:41:10 +0100

    loop

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 "use strict";
-
+let container_width;
+let container_height;
 const imageUrl = document.querySelector("#image").value;
 
 document.addEventListener("DOMContentLoaded", startScript);
@@ -11,4 +12,33 @@ function startScript() {
 
 function loadImage() {
   document.querySelector("img").src = imageUrl;
+  document.querySelector("img").onload = imageErLoaded;
+  console.log("billedet bliver vist!");
+}
+
+const numOfX = document.querySelector("#x").value;
+const numOfY = document.querySelector("#y").value;
+
+function imageErLoaded() {
+  document.querySelector("img").naturalWidth;
+  container_width = document.querySelector("img").naturalWidth;
+  container_height = document.querySelector("img").naturalHeight;
+  createDropZones();
+}
+
+function createDropZones() {
+  document.querySelector(
+    "#container"
+  ).style.gridTemplateColumns = `repeat(${numOfX}, 1fr)`;
+  document.querySelector("#container").style.width = `${container_width}px`;
+  document.querySelector("#container").style.height = `${container_height}px`;
+  for (let y = 0; y < numOfY; y++) {
+    for (let x = 0; x < numOfX; x++) {
+      let piece = document.createElement("div");
+      piece.style.height = container_height / numOfY + "px";
+      piece.textContent = `${x}${y}`;
+      piece.classList.add("piece");
+      document.querySelector("#container").appendChild(piece);
+    }
+  }
 }
